a9695221e7b3c4eb6452d7952e539b786761197e
Joe Mullen <mullenjoe13@gmail.com>
2018-11-23 15:52:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,7 +2,7 @@ var playlist = {
   cocteauTwins: 'pearly dewdrops drops'
 }
 
-function updatePlaylist(playlist, artist, song)  {
-  playlist[artist] = value
+function updatePlaylist(playlist, artistName, songTitle)  {
+  playlist[artistName] = songTitle
   return playlist
 }
